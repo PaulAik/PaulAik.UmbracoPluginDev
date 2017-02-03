06b88ecbae801c280e4045409b96234b73950546
Paul Aikman <paul.aikman@eqtr.com>
2017-02-03 13:33:07 +0000

    - Prevalue checkboxes now work! - Git ignore updates

--- UmbracoPluginDev/App_Plugins/UltimateUrlPicker/prevalues/allowedmodes.js
+++ UmbracoPluginDev/App_Plugins/UltimateUrlPicker/prevalues/allowedmodes.js
@@ -1,24 +1,6 @@
 ﻿angular.module("umbraco").controller("PaulAik.UltimatePicker.AllowedModesController",
     function ($scope) {
 
-        if (angular.isObject($scope.model.config.items)) {
-
-            //now we need to format the items in the dictionary because we always want to have an array
-            var newItems = [];
-            var vals = _.values($scope.model.config.items);
-            var keys = _.keys($scope.model.config.items);
-            for (var i = 0; i < vals.length; i++) {
-                newItems.push({ id: keys[i], sortOrder: vals[i].sortOrder, value: vals[i].value });
-            }
-
-            //ensure the items are sorted by the provided sort order
-            newItems.sort(function (a, b) { return (a.sortOrder > b.sortOrder) ? 1 : ((b.sortOrder > a.sortOrder) ? -1 : 0); });
-
-            //re-assign
-            $scope.model.config.items = newItems;
-
-        }
-
         function setupViewModel() {
             $scope.selectedItems = [];
 
@@ -28,19 +10,33 @@
                 $scope.model.value = [];
             }
 
-            for (var i = 0; i < $scope.model.config.items.length; i++) {
-                var isChecked = _.contains($scope.model.value, $scope.model.config.items[i].id);
-                $scope.selectedItems.push({
-                    checked: isChecked,
-                    key: $scope.model.config.items[i].id,
-                    val: $scope.model.config.items[i].value
-                });
-            }
+            $scope.selectedItems.push({
+                checked: _.contains($scope.model.value, 'url'),
+                key: 'url',
+                val: 'URL'
+            });
+
+            $scope.selectedItems.push({
+                checked: _.contains($scope.model.value, 'content'),
+                key: 'content',
+                val: 'Content'
+            });
+
+            $scope.selectedItems.push({
+                checked: _.contains($scope.model.value, 'media'), 
+                key: 'media',
+                val: 'Media'
+            });
+
+            $scope.selectedItems.push({
+                checked: _.contains($scope.model.value, 'upload'), 
+                key: 'upload',
+                val: 'Upload'
+            });
 
         }
 
         setupViewModel();
-
 
         //update the model when the items checked changes
         $scope.$watch("selectedItems", function (newVal, oldVal) {
@@ -60,5 +56,4 @@
             //update the display val again if it has changed from the server
             setupViewModel();
         };
-
     });
