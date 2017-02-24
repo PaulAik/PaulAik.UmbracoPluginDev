2bed5ca6f6f0ebc42c43bf00359630ea9cf1a2d8
Paul Aikman <paul.aikman@eqtr.com>
2017-02-24 16:56:16 +0000

    - Started working on grid values

---
+++ ClassicDataTypeGrid/PropertyEditors/ClassicDataTypeGridPropertyValueEditorWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+using Umbraco.Web.PropertyEditors;
+
+namespace ClassicDataTypeGrid.PropertyEditors
+{
+    public class ClassicDataTypeGridPropertyValueEditorWrapper : PropertyValueEditorWrapper
+    {
+        public ClassicDataTypeGridPropertyValueEditorWrapper(PropertyValueEditor wrapped) : base(wrapped)
+        {
+        }
+
+        public override string ConvertDbToString(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
+        {
+            
+
+            return base.ConvertDbToString(property, propertyType, dataTypeService);
+        }
+    }
+}
